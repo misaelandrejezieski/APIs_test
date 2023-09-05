28ff0a42ff33bab599cf0da3de30bc11a968c1f1
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-09-05 18:38:02 -0300

    up Spring

---
+++ API_test/API_test/src/test/java/br/com/API_test/API_test/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.API_test.API_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
