f3413baa7889893104f131c9f76c48b74e0fda95
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-09-06 16:24:58 -0300

    Update ApiTestApplication.java

--- API_test/API_test/src/main/java/br/com/API_test/API_test/ApiTestApplication.java
+++ API_test/API_test/src/main/java/br/com/API_test/API_test/ApiTestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiTestApplication {
+	// o código não esta funcionando.
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTestApplication.class, args);
