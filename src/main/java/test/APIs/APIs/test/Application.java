6b29e8b9082cfd44a68ce55b92517d741e256007
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-09-11 20:00:50 -0300

    Update Application.java

--- src/main/java/test/APIs/APIs/test/Application.java
+++ src/main/java/test/APIs/APIs/test/Application.java
@@ -1,7 +1,9 @@
 package test.APIs.APIs.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
