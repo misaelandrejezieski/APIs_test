5866ff649adef54e1755a71efaa48a7a184fd6cc
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-09-11 19:34:56 -0300

    eclipse

---
+++ src/test/java/test/APIs/APIs/test/Api.java
@@ -0,0 +1,14 @@
+package test.APIs.APIs.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Api {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
